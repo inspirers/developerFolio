77bd9e0a610bff080826cd80c81e04af1efe08b7
Arvid Boisen <arvid.boisen@protonmail.com>
2023-02-04 15:18:24 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ npm install --production
 npm audit fix
 npm run build
 sudo rm /var/www/html/cv/* -r
-cp /home/pi/developerFolio/build/* /var/www/html/cv -r
+sudo cp /home/pi/developerFolio/build/* /var/www/html/cv -r
